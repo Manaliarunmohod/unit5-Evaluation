cf4764f00443245ddddabffc82c9da1c0bf7cb58
Manali mohod <masaimanali@gmail.com>
2022-04-18 15:04:22 +0530

    changes

---
+++ eval2/src/components/AddStudent.jsx
@@ -0,0 +1,36 @@
+import {useState} from "react";
+import axios from "axios";
+
+function AddStudent({toggleDisplay}) {
+
+    const [formData, setFormData] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        gender: "",
+        age: "",
+        tenth_score: "",
+        twelth_score: "",
+        preferred_branch: ""
+    })
+
+    function handleChange(e) {
+        //let {cla}
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault;
+
+        axios.post("http://localhost:8080/students", formData).then(toggleDisplay());
+    }
+
+
+
+
+    return (
+<diV></diV>
+    )
+}
+
+
+export {AddStudent};
