99cad1863886a6b5202465192b3f1de7b453d564
Manali mohod <masaimanali@gmail.com>
2022-04-18 15:59:13 +0530

    form

---
+++ eval2/src/components/ShowStudent.jsx
@@ -0,0 +1,17 @@
+function ShowStudent({data, setData}){
+
+    //const [text, setText] = useState("");
+
+    return (
+        <div className="container">
+            <div className="subCon">
+                <button onClick={()=> {
+                    //let newarr = [...data]
+
+                }}>click</button>
+            </div>
+        </div>
+    )
+}
+
+export {ShowStudent};
